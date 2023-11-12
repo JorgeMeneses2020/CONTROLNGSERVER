c1ed838ebb49d387c45b85288e83596a8feac05c
Jorge Meneses <jorgeluis.menesespaternina@gmail.com>
2023-11-12 12:49:39 -0500

    ACTUALIZACIÓN

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,34 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <HTTPClient.h>
 
 // ********FIN IMPORTE DE LIBRERÍAS***********//
 
 // ********DECLARACIÓN DE VARIABLES***********//
 // const char *ssid = "GobantEdu2023";
 // const char *password = "";
-const char *ssid = "";
-const char *password = "";
+const char *ssid = "Oszanet_meneses";
+const char *password = "jorge890";
 
-const char *mqtt_server = "ngiot2023.online";
+const char *mqtt_server = "automaticiot.cloud";
 const int mqtt_port = 1883;
-const char *mqtt_user = "";
-const char *mqtt_pass = "";
-
+const char *mqtt_user = "web_client";
+const char *mqtt_pass = "121212";
+ String macAddress = "";
+  String macAddressWeb = "";
 long lastMsg = 0;
 char msg[25];
+char msg2[25];
 bool autom = false;
 int temp = 0;
 int hums = 0;
 int huma = 0;
 int humMin = 60;
-
+bool enviar=false;
 #define DHTPIN 0
 #define HUMSPIN 36
-#define DHTTYPE DHT22
+#define DHTTYPE DHT11
 
 // ********FIN DECLARACIÓN DE VARIABLES***********//
 
@@ -60,6 +63,14 @@ void setup()
   pinMode(16, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(17, OUTPUT);
+   uint8_t mac[6];
+   WiFi.macAddress(mac);  
+  for (int i = 0; i < 6; ++i) {
+    macAddress += String(mac[i], 16);
+    if (i < 5) {
+      macAddress += ':';
+    }
+  }
 }
 
 // ********CONEXIÓN DEL WIFI A LA RED***********//
@@ -100,57 +111,130 @@ void callback(char *topic, byte *payload, unsigned int length)
   }
   incoming.trim();
   Serial.println("Mensaje ->" + incoming);
-
-  if ((String)topic == "humedadMinima")
-  {
-    int valHumMin = incoming.toInt();
-    humMin = valHumMin;
-    Serial.print("Humedad minima");
-    Serial.println(humMin);
-  }
-  if (incoming == "on")
-  {
+    if((String)topic=="mac"){
+      macAddressWeb=(String)incoming;
+      Serial.println(macAddressWeb);
+      Serial.println(macAddress);
+      delay(500);
+      enviar=true;
+      if(macAddressWeb=="desconectado" || macAddress!=macAddressWeb){
+        enviar=false;
+        Serial.println("DESCONECTADO");
+      }else{
+        if(macAddress==macAddressWeb){
+          enviar=true;
+        }
+        
+      }
+      
+    }
+     Serial.print(topic);
+      if (incoming == "on")
+    {
     if ((String)topic == "relay_3")
     {
-      autom = true;
-      client.publish("alerta", "automatico encendido");
+        if(macAddress==macAddressWeb){
+           autom = true;
+          String alertMac=String("Modo auto encendido") + ","+String(macAddress);
+          alertMac.toCharArray(msg2,50);
+          client.publish("alerta",msg2);    
+        }
+     
     }
-    Serial.print(topic);
     if ((String)topic == "relay_1")
     {
+       
+     if(macAddress==macAddressWeb){
       digitalWrite(5, HIGH);
-      client.publish("alerta", "Rele 1 encendido");
-
+      String alertMac=String("Rele 1 encendido") + ","+String(macAddress);
+      alertMac.toCharArray(msg2,50);
+       client.publish("alerta",msg2);      
       Serial.println("Encendido correactamente r");
+     }
+      
+     
     }
     if ((String)topic == "relay_2")
     {
-      digitalWrite(17, HIGH);
-      client.publish("alerta", "Rele 2 encendido");
+        if(macAddress==macAddressWeb){
+            digitalWrite(17, HIGH);
+       String alertMac=String("Rele 2 encendido") + ","+String(macAddress);
+      alertMac.toCharArray(msg2,50);
+       client.publish("alerta",msg2);    
       Serial.println("Encendido correactamente r");
+        }
+    
     }
   }
   if (incoming == "off")
   {
     if ((String)topic == "relay_1")
     {
-      digitalWrite(5, LOW);
-      client.publish("alerta", "Rele 1 apagado");
+      if(macAddress==macAddressWeb){
+      digitalWrite(5, LOW);     
       Serial.println("Apagado correactamente r");
+      String alertMac=String("Rele 1 apagado") + ","+String(macAddress);
+      alertMac.toCharArray(msg2,50);
+       client.publish("alerta",msg2);      
+      }
     }
     if ((String)topic == "relay_2")
     {
-      digitalWrite(17, LOW);
-      client.publish("alerta", "Rele 2 apagado");
+       if(macAddress==macAddressWeb){
+          digitalWrite(17, LOW);
+        String alertMac=String("Rele 2 apagado") + ","+String(macAddress);
+      alertMac.toCharArray(msg2,50);
+       client.publish("alerta",msg2);    
+      Serial.println("Encendido correactamente r");
+       }
+    
 
-      Serial.println("Apagado correactamente r");
+     
     }
     if ((String)topic == "relay_3")
     {
+        if(macAddress==macAddressWeb){
       autom = false;
-      client.publish("alerta", "automatico apagado");
+          String alertMac=String("Modo auto apagado") + ","+String(macAddress);
+          alertMac.toCharArray(msg2,50);
+          client.publish("alerta",msg2);    
+        }
+     
     }
   }
+    
+// REVISAR PROGRAMACIÓN DE HUMEDAD MÍNIMA
+  if ((String)topic == "humedadMinima")
+  {
+    char datoCapturado[20]; 
+     char macHumMin[30]; 
+     if(macAddress==macAddressWeb){
+      char valHumMin[27];
+      incoming.toCharArray(valHumMin,30);
+       char tokens[2][30];
+
+      char *token =strtok(valHumMin,",");
+      int i=0;
+      while(token!=NULL && i< 2 ){
+        strcpy(tokens[i], token);
+         Serial.println(tokens[i]);
+         token = strtok(NULL, ",");
+         i++;
+      }
+       strcpy(datoCapturado, tokens[0]);
+       strcpy(macHumMin, tokens[1]);
+    
+       String newMac=macHumMin;
+if(macAddress==newMac){
+   humMin=atoi(datoCapturado);
+    Serial.print("Humedad minima");
+    Serial.println(humMin);
+}
+  
+     }
+   
+  }
+ 
 }
 // ******** FIN RECEPCIÓN DE MENSAJES DEL BROKER***********//
 
@@ -174,6 +258,7 @@ void reconnect()
       client.subscribe("relay_3");
       client.subscribe("relay_4");
       client.subscribe("humedadMinima");
+      client.subscribe("mac");
     }
     else
     {
@@ -188,20 +273,25 @@ void reconnect()
 
 void automatico()
 {
-
+    Serial.println(hums);
+    Serial.println(humMin);
+   
   if (hums <= humMin)
   {
     digitalWrite(5, HIGH);
+    Serial.println("Riego activado");
   }
   else
   {
     digitalWrite(5, LOW);
+     Serial.println("Riego apagado");
   }
 }
 
 // ******** FIN RECONONEXIÓN AL  BROKER***********//
 void loop()
 {
+  
   if (!client.connected())
   {
     if (WiFi.status() != WL_CONNECTED)
@@ -221,6 +311,7 @@ void loop()
     if (isnan(event.temperature))
     {
       Serial.println(F("Error reading temperature!"));
+   
     }
     else
     {
@@ -228,11 +319,15 @@ void loop()
       Serial.print(F("Temperature: "));
       Serial.print(event.temperature);
       Serial.println(F("°C"));
+      // Serial.println(macAddressWeb);
+      // Serial.println(macAddress);
     }
     dht.humidity().getEvent(&event);
     if (isnan(event.relative_humidity))
     {
       Serial.println(F("Error reading humidity!"));
+      // Serial.println(macAddressWeb);
+      // Serial.println(macAddress);
     }
     else
     {
@@ -241,17 +336,23 @@ void loop()
       Serial.println(F("%"));
       huma = event.relative_humidity;
     }
+   
     if (autom)
     {
       automatico();
     }
     hums = analogRead(HUMSPIN);
     hums = map(hums, 4095, 0, 0, 100);
+   
+ 
 
-    String to_send = String(temp) + "," + String(hums) + "," + String(huma);
-    to_send.toCharArray(msg, 25);
+    String to_send = String(temp) + "," + String(hums) + "," + String(huma)+ "," + macAddress;
+    to_send.toCharArray(msg, 30);
     Serial.print("Publicación del mensaje");
-    Serial.println(msg);
-    client.publish("values", msg);
+    if(enviar){
+          client.publish("values", msg);
+              Serial.println(msg);
+    }
+
   }
 }
